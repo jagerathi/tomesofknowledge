14297614cdae4dbae5e1e837d4e33e3cfbd11964
jagerati <jagerathi@gmail.com>
2020-12-02 13:14:46 -0500

    fixed the paging issue with component-list

--- codingtests/codingchallenge_starz/FullStack/ClientApp/src/app/change/change.component.ts
+++ codingtests/codingchallenge_starz/FullStack/ClientApp/src/app/change/change.component.ts
@@ -20,9 +20,8 @@ export class ChangeComponent {
   headers = new HttpHeaders().set('Content-Type', 'application/json').set('Accept', 'application/json');
   httpOptions = {
     headers: this.headers
-  };
-  baseUrl: string;
-
+  }
+  
   constructor(private fb: FormBuilder, private http: HttpClient, @Inject('BASE_URL') baseUrl: string) { 
     this.headers = new HttpHeaders({ 'Content-Type': 'application/json' });
   }
@@ -36,7 +35,7 @@ export class ChangeComponent {
     // TODO Number 4 Call the UpdateComponent API
     console.warn(this.uploadForm.value);
 
-    this.http.patch(this.baseUrl + '/api/components/${id}', JSON.stringify({ Status: status}));
+    //this.http.patch(this.baseUrl + '/api/components/${id}', JSON.stringify({ Status: status}));
   }    
 }
 
--- codingtests/codingchallenge_starz/FullStack/ClientApp/src/app/components/component-list.component.ts
+++ codingtests/codingchallenge_starz/FullStack/ClientApp/src/app/components/component-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-component-list',
@@ -14,12 +14,79 @@ export class ComponentListComponent {
     headers: this.headers
   };
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-   
+  pageNumber: number;
+  pageSize: number;
+  url: string;
 
-    http.get<ListComponentResponse>(baseUrl + 'api/components' ).subscribe(result => {
+  constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+   
+    this.pageNumber = 0;
+    this.pageSize = 10;
+    this.url = baseUrl + 'api/components';
+
+    let params = new HttpParams().set("PageSize", this.pageSize.toString()).set("PageNumber", this.pageNumber.toString());
+
+    http.get<ListComponentResponse>(this.url, { headers: this.headers, params: params } ).subscribe(result => {
       this.components = result.components;
     }, error => console.error(error));
+  }
+
+  nextPage() {
+
+    this.pageNumber++;
+
+    let params = new HttpParams().set("PageSize", this.pageSize.toString()).set("PageNumber", this.pageNumber.toString());
+
+    this.http.get<ListComponentResponse>(this.url, { headers: this.headers, params: params } ).subscribe(result => {
+      this.components = result.components;
+    }, error => console.error(error));
+  }
+
+  prevPage() {
+
+    if(this.pageNumber > 0)
+    {
+      this.pageNumber--;
+
+      let params = new HttpParams().set("PageSize", this.pageSize.toString()).set("PageNumber", this.pageNumber.toString());
+
+      this.http.get<ListComponentResponse>(this.url, { headers: this.headers, params: params } ).subscribe(result => {
+        this.components = result.components;
+      }, error => console.error(error));
+    }
+  }
+
+  sort(index: number) {
+    this.pageNumber = 0;
+
+    var sortCol = "id";
+    var sortDir = "asc";
+
+    switch(index) 
+    {
+      case 0:
+        sortCol = "id";
+        sortDir = "asc";
+        break;
+      case 1:
+        sortCol = "name";
+        sortDir = "asc";
+        break;
+      case 2:
+        sortCol = "type";
+        sortDir = "asc";
+        break;
+      case 3:
+        sortCol = "status";
+        sortDir = "asc";
+        break;
+    }
+
+    let params = new HttpParams().set("SortDir", sortDir).set("SortField", sortCol);
+
+      this.http.get<ListComponentResponse>(this.url, { headers: this.headers, params: params } ).subscribe(result => {
+        this.components = result.components;
+      }, error => console.error(error));
   }
 }
 
